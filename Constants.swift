b3c6a99012618fcdc7682f572723f7231a7f4f91
Michael Doroff <michael_doroff@yahoo.com>
2017-07-17 21:03:16 -0500

    Added Constants file

---
+++ Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Brews: An Introduction
+//
+//  Created by Michael Doroff on 7/17/17.
+//  Copyright © 2017 Michael Doroff. All rights reserved.
+//
+
+import Foundation
+
+extension BreweryDBCLient {
+    
+    
+    struct Constants {
+        
+        static let APIKey = "7d64d2655fe3c2f90b82dae866dea77b"
+    }
+    
+    
+    
+}
